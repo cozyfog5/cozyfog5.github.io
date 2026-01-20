f62bcc1307687615323be60c91bef44234048c60
cozyfog5 <255960134+cozyfog5@users.noreply.github.com>
2026-01-20 02:50:21 -0800

    implement unrefined version of calc alg
    Now interprets 24 names and 24 scoreboard entries, performs MMR calcs, and prints them to the output. There's a lot more to fix and refine, but this is a major milestone regardless.

--- script.js
+++ script.js
@@ -71,14 +71,72 @@ function calculateMmrAdjustment(mmrA, mmrB, decision) {
   }
 }
 
+// Interprets text as the mogi "header," which I define to be the first message posted in the room,
+// and returns a mapping from name to starting MMR if possible.
+function parseHeaderInput(input) {
+  let nameMmrMap = new Map();
+  let linesArr = input.split(/\r?\n/);
+  for (const line of linesArr) {
+    let nameTest = line.match(/^`\d+\.` (.+) \((\d+) MMR\)$/);
+    if (nameTest) {
+      // Replace is needed for backslashes that get added before punctuation marks otherwise used
+      // for Discord formatting (including _underscores_, which appear frequently).
+      nameMmrMap.set(nameTest[1].replace(/\\/g, ""), Number(nameTest[2]));
+    }
+  }
+  return nameMmrMap;
+}
+
+// Interprets the scoreboard and returns a mapping from name to finishing score.
+function parseScoreboardInput(input) {
+  let nameScoreMap = new Map();
+  let linesArr = input.split(/\r?\n/);
+  for (const line of linesArr) {
+    let scoreTest = line.match(/^(.+) (\d+)$/);
+    if (scoreTest) {
+      // Replace is needed for backslashes that get added before punctuation marks otherwise used
+      // for Discord formatting (including _underscores_, which appear frequently).
+      nameScoreMap.set(scoreTest[1].replace(/\\/g, ""), scoreTest[2]);
+    }
+  }
+  return nameScoreMap;
+}
+
 function processText(a, b) {
-  return [
-    '--- Input A ---',
-    a.trim(),
-    '',
-    '--- Input B ---',
-    b.trim(),
-    '',
-    '--- End ---'
-  ].join('\n');
+  let nameMmrMap = parseHeaderInput(a);
+  let nameScoreMap = parseScoreboardInput(b);
+  let placeNameMap = new Map();
+
+  // Validate maps: Make sure each has 24 entries and identical keys.
+  if (nameMmrMap.size != 24) {
+    setStatus("Detected " + nameMmrMap.size + " unique names instead of the expected 24. (This tool currently supports only 24-player FFA formats.)");
+    return;
+  }
+  if (nameScoreMap.size != 24) {
+    setStatus("Detected " + nameScoreMap.size + " unique scoreboard entries instead of the expected 24.");
+    return;
+  }
+
+  // TODO: Add validation to ensure names are 1:1 between maps.
+
+  let mmrAdjustmentText = "```Expected MMR Changes (unofficial)\n";
+  for (const [name1, mmr1] of nameMmrMap) {
+    let mmrAdjustment = 0;
+    let place = 1;
+    let mmrAfter = 0;
+    for (const [name2, mmr2] of nameMmrMap) {
+      const decision = Math.sign(nameScoreMap.get(name1) - nameScoreMap.get(name2));
+      place += (decision === -1 ? 1 : 0);
+      mmrAdjustment += calculateMmrAdjustment(mmr1, mmr2, decision);
+    }
+    mmrAdjustment = Math.round(mmrAdjustment);
+    mmrAfter = Math.ceil(mmr1 + mmrAdjustment, 0);
+    mmrAdjustment = mmrAfter - mmr1;
+
+    mmrAdjustmentText += name1 + ": " + mmr1 + " --> " + mmrAfter + " (" + mmrAdjustment + ")\n";
+  }
+
+  mmrAdjustmentText += "\nMade at https://cozyfog5.github.io```";
+
+  return mmrAdjustmentText;
 }

0cd498bc458ae729355fdd95f5fa700c267f1daf
cozyfog5 <255960134+cozyfog5@users.noreply.github.com>
2026-01-20 00:51:09 -0800

    implement mmr calc function

--- script.js
+++ script.js
@@ -54,6 +54,23 @@ document.addEventListener('keydown', (event) => {
   }
 });
 
+// Decision: if 1, Player A wins; if -1, player B wins; if 0, Players A and B tie
+function calculateMmrAdjustment(mmrA, mmrB, decision) {
+  const cap = 30;
+  const logistic_base = 11;
+  const mmr_delta_denom = 9500;
+  const baseline = 10;
+  const offset = Math.log(cap / baseline - 1) / Math.log(logistic_base);
+
+  if (decision === 0) {
+    // Tie
+    return Math.sign(mmrB - mmrA) * (cap / (1 + Math.pow(logistic_base, -(Math.abs(mmrB - mmrA) / mmr_delta_denom - offset))) - cap / 3);
+  } else {
+    // Decision
+    return Math.sign(decision) * cap / (1 + Math.pow(logistic_base, -(Math.sign(decision) * (mmrB - mmrA) / mmr_delta_denom - offset)));
+  }
+}
+
 function processText(a, b) {
   return [
     '--- Input A ---',

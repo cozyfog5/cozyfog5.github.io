2813a891642c43874d0c0292897299cdada65612
cozyfog5 <255960134+cozyfog5@users.noreply.github.com>
2026-03-03 12:12:35 -0800

    allow mmr delta to change width
    In the (unlikely) scenario that all MMR deltas are less than 4 characters wide, the space allocated for the MMR deltas will now adapt

--- script.js
+++ script.js
@@ -211,6 +211,8 @@ function getMmrChangeSummaryText(playersInfo) {
   let longestNameLength = 0;
   let longestMmrBeforeLength = 0;
   let longestMmrAfterLength = 0;
+  let longestMmrDeltaLength = 0;
+  let mmrDeltaTextMap = new Map();
 
   // Record lengths of names and MMRs
   for (const player of playersInfo) {
@@ -223,15 +225,20 @@ function getMmrChangeSummaryText(playersInfo) {
     if (String(player.mmrAfter).length > longestMmrAfterLength) {
       longestMmrAfterLength = String(player.mmrAfter).length;
     }
+    let mmrDeltaText = (player.mmrChange > 0 ? "+" : "") + player.mmrChange;
+    if (mmrDeltaText.length > longestMmrDeltaLength) {
+      longestMmrDeltaLength = mmrDeltaText.length;
+    }
+    mmrDeltaTextMap.set(player.name, mmrDeltaText);
   }
   
   let mmrAdjustmentText = "```Expected MMR Changes (unofficial)";
   for (const player of playersInfo) {
-    let mmrChangeText = (player.mmrChange > 0 ? "+" : "") + player.mmrChange;
+    let mmrDeltaText = mmrDeltaTextMap.get(player.name);
     mmrAdjustmentText += "\n" + player.name + " ".repeat(longestNameLength - player.name.length) + ": " +
         " ".repeat(longestMmrBeforeLength - String(player.mmrBefore).length) + player.mmrBefore + " --> " +
         " ".repeat(longestMmrAfterLength - String(player.mmrAfter).length) + player.mmrAfter + " (" +
-        " ".repeat(4 - mmrChangeText.length) + mmrChangeText + ")";
+        " ".repeat(longestMmrDeltaLength - mmrDeltaText.length) + mmrDeltaText + ")";
   }
 
   mmrAdjustmentText += "```\n-# Generated using https://cozyfog5.github.io. No warranty implied. May contain errors.";
